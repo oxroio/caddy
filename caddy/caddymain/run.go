fd242329cd69a7e02b94d69ed6f19fefd7afd693
Edward Jibson <jibson@tuta.io>
2019-09-29 23:43:59 +0100

    Update run.go

--- caddy/caddymain/run.go
+++ caddy/caddymain/run.go
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 package caddymain
-
+import _ "github.com/caddyserver/dnsproviders/cloudflare"
 import (
 	"bufio"
 	"errors"
@@ -42,7 +42,6 @@ import (
 	// This is where other plugins get plugged in (imported)
         _ "github.com/BTBurke/caddy-jwt"
 	_ "github.com/tarent/loginsrv/caddy"
-	_ "github.com/oxroio/dnsproviders/cloudflare"
 )
 
 func init() {
